8c243cfe62bb78d44e6bb73db7733e9f084ce103
Sagar Akuch <sagarakuch00@gmail.com>
2025-07-28 16:44:25 +0530

    Learning About Middleware

---
+++ MiddlewareDemo/Middlewares/CustomMiddleware1.cs
@@ -0,0 +1,21 @@
+﻿namespace MiddlewareDemo.Middlewares
+{
+    public class CustomMiddleware1
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware1(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Custom midddleware 1 Executed in Request");
+
+            await _next(context);
+
+            Console.WriteLine("Custom Middleware 1 Executed in Response");
+        }
+    }
+}
---
+++ MiddlewareDemo/Middlewares/CustomMiddleware2.cs
@@ -0,0 +1,27 @@
+﻿namespace MiddlewareDemo.Middlewares
+{
+    public class CustomMiddleware2
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware2(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Custom midddleware 2 Executed in Request");
+
+            //if (context.Request.Method == "GET")
+            //{
+            //    context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+            //    return; // short circuit Middleware
+            //}
+
+            await _next(context); // call next middleware
+
+            Console.WriteLine("Custom midddleware 2 Executed in Request");
+        }
+    }
+}
